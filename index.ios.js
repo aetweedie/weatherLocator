3bd5a0abc339ee1c0c9017873bb5861dfc061a77
Alan Tweedie <alan.tweedie@echostar.com>
2017-08-08 10:08:29 -0600

    Took out bg

--- index.ios.js
+++ index.ios.js
@@ -25,13 +25,12 @@ export default class weatherLocator extends Component {
       renderText: true,
       text: '',
       weather: {},
-      bg: '',
-      weatherData: ''
+      weatherData: '',
+      iconUrl: ''
     };
   }
 
   getWeather = () => {
-      this.setState({bg: ''})
       let zip = this.state.text;
       let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=63a6f4c343335f65e9803ad48c03abf6&units=imperial";
       console.log(url);
@@ -43,32 +42,20 @@ export default class weatherLocator extends Component {
         .catch((error) => console.log(error))
   }
 
-  handleClouds = (clouds) => {
-    if (clouds > 80) {
-      this.setState({bg: images.overcast});
-    } else if (clouds > 60) {
-      this.setState({bg: images.mostly});
-    } else if (clouds > 40) {
-      this.setState({bg: images.partly});
-    } else if (clouds > 20){
-      this.setState({bg: images.someClouds});
-    } else {
-      this.setState({bg: images.sunny});
-    }
-  }
 
   componentWillMount(){
     const weatherData = this.state.weather;
-    let iconUrl, bg;
+    let iconUrl;
     if (weatherData.weather) {
       const weatherIcon = weatherData.weather[0];
       iconUrl = "http://openweathermap.org/img/w/" + weatherIcon.icon + ".png";
-      if (this.state.bg) this.handleClouds(weatherData.clouds.all);
+      this.setState({iconUrl: iconUrl});
     }
   }
 
   render() {
     let weatherData = this.state.weather;
+    let iconUrl;
     console.log(this.state);
     return (
       <View style={styles.container}>
@@ -88,7 +75,7 @@ export default class weatherLocator extends Component {
             {'\n'}
             Current temp: {weatherData.main.temp}°
             {'\n'}
-            <Image style={{width: 100, height: 100}} source={this.state.bg}></Image>
+            <Image style={{width: 100, height: 100}} source={iconUrl}></Image>
           </Text>
           : <Text>No data... yet!</Text>
         }
